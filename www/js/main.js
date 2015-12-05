d8fd3af7e57935a6b1dbce289eed55c30b5b1854
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2015-12-05 01:22:13 +0100

    start with main

--- www/js/main.js
+++ www/js/main.js
@@ -3,10 +3,8 @@ var currentWiese = null;
 
 var init = function(){
 
-	//if(currentWiese == null){
-	//	new Login();
-	//}
+	if(currentWiese == null){
+		new Login();
+	}
 
-	tree_form = new TreeForm(new Wiese());
-	tree_form.show_form();
 };
