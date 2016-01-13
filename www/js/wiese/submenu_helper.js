216ebd7aa8809e7466fc045a5d30d0be05335429
F4b1- <fabian.gand@gmx.net>
2016-01-13 18:14:18 +0100

    added filters

--- www/js/wiese/submenu_helper.js
+++ www/js/wiese/submenu_helper.js
@@ -1,175 +1,206 @@
-var WieseSubmenuHelper = function(){
-	this.wiese = null;
-	this.trees = null;
+{
+	var WieseSubmenuHelper = function(){
+		this.wiese = null;
+		this.trees = null;
 
-	this.breadcrumb_id = "sub_menu_breadcrumb";
-	this.boxes_id = "sub_box_menu"
+		this.breadcrumb_id = "sub_menu_breadcrumb";
+		this.boxes_id = "sub_box_menu"
 
-	this.menus = {
-		filter: {submenu: "filter"},
-		baume: {submenu: "baume"},
-		info: {submenu: "info"}
-	}
+		this.menus = {
+			filter: {submenu: "filter"},
+			baume: {submenu: "baume"},
+			info: {submenu: "info"}
+		}
 
-	this.menu_keys = function(){
-		return Object.keys(this.menus);
-	}
-};
-
-WieseSubmenuHelper.prototype.set_wiese = function(wiese){this.wiese = wiese };
-
-WieseSubmenuHelper.prototype.set_trees = function(trees){this.trees = trees };
-
-WieseSubmenuHelper.prototype.set_size_of_box = function(){
-	//make box coorect size
-	var window_height = $(window).height();
-	var side_bar_offset = $('#header_submenu').offset().top;
-	var menu_header_height = $('#header_submenu').outerHeight();
-	$('#sub_box_menu').height(window_height - side_bar_offset - menu_header_height - 35);
-}
-
-WieseSubmenuHelper.prototype.fill = function(){
-	this.set_size_of_box();
-	//fill the submenus with data
-	this.fill_info_box();
-	this.fill_baume_box();
-	this.fill_filter_box();
-}
-
-WieseSubmenuHelper.prototype.fill_info_box = function(){
-	$('#wiesenName').html(this.wiese.name);
-
-	var info_box = $('#wiesen_info_box');
-
-	if(this.wiese.getArea()){
-		info_box.append($('<p/>',{html: "Flächeninhalt: " + this.wiese.getArea() + " m" + "<sup>2</sup>"}));
+		this.menu_keys = function(){
+			return Object.keys(this.menus);
+		}
 	};
 
-	if(this.trees){
-		var len = Object.keys(this.trees).length;
-		info_box.append($('<p/>', {html: "Anzahl B&auml;ume: " + len}))
-	}
-	
-	var newestTimestamp = 0;
-	for (var key in this.trees) {
-   var obj = this.trees[key];
-   if (obj.timestamp > newestTimestamp) {
-	newestTimestamp = obj.timestamp;
-   }
-}
-	var dateUpdated = new Date( newestTimestamp);
-	
-	
-	var dateCreated = new Date( this.wiese.data.timestamp);
-	
-	info_box.append($('<p>', {text: "Zuletzt betrachtet: " + dateUpdated.toGMTString()}));
-	info_box.append($('<p/>', {text: "Erstellt am " +  dateCreated.toGMTString()}));
-	info_box.append($('<p/>', {text: "Wetter??"}));
+	WieseSubmenuHelper.prototype.set_wiese = function(wiese){this.wiese = wiese };
 
-	if(sessionStorage.getItem('user') == 'Offline') {
-		$('#buttonOrchardOffline').attr('disabled', 'disabled' );
+	WieseSubmenuHelper.prototype.set_trees = function(trees){this.trees = trees };
+
+	WieseSubmenuHelper.prototype.set_size_of_box = function(){
+		//make box coorect size
+		var window_height = $(window).height();
+		var side_bar_offset = $('#header_submenu').offset().top;
+		var menu_header_height = $('#header_submenu').outerHeight();
+		$('#sub_box_menu').height(window_height - side_bar_offset - menu_header_height - 35);
 	}
 
-	$('#buttonOrchardOffline').click(function(){
-		this.wiese.save_map_for_offline_use();
-		makeAvailableOffline(this.wiese.name, this.wiese.data);
-		$('#myModalOrchard').modal('show');
-	}.bind(this));
-};
-
-WieseSubmenuHelper.prototype.fill_baume_box = function(){
-	var renderd_trees = RenderHelper.get_renderd_trees(this.wiese);
-	var ele;
-
-	if(renderd_trees.length > 0){
-		ele = $('<div/>', {class: "list-group"});
-
-		renderd_trees.forEach(function(rend_tree){
-			ele.append(rend_tree);
-		});
-
-	}else{
-		//wiese besitzt keine baume
-		ele = $('<div/>', {class: "well", text: "Sie haben noch keine Bäume auf ihrer Wiese plaziert"});
+	WieseSubmenuHelper.prototype.fill = function(){
+		this.set_size_of_box();
+		//fill the submenus with data
+		this.fill_info_box();
+		this.fill_baume_box();
+		this.fill_filter_box();
+		$('#checkboxAll').click();
+		$('input[type="checkbox"]').change(function () {
+	    if ($('input[type="checkbox"]').is(':checked')) {
+		this.wiese.place_trees_on_map(goOverCheckboxes(this.wiese));
+	    } else {
+		this.wiese.place_trees_on_map(goOverCheckboxes(this.wiese));
+	    }
+		}.bind(this));
+		
 	}
 
-	$('#trees_list').html(ele)
-}
+	WieseSubmenuHelper.prototype.fill_info_box = function(){
+		$('#wiesenName').html(this.wiese.name);
 
-WieseSubmenuHelper.prototype.fill_filter_box = function(){
+		var info_box = $('#wiesen_info_box');
 
-}
+		if(this.wiese.getArea()){
+			info_box.append($('<p/>',{html: "Flächeninhalt: " + this.wiese.getArea() + " m" + "<sup>2</sup>"}));
+		};
 
-WieseSubmenuHelper.prototype.get_submenu_btn = function(menu){
-	var data_attr = menu.submenu;
-	return $('*[data-submenu="' +  data_attr + '"]');
-}
+		if(this.trees){
+			var len = Object.keys(this.trees).length;
+			info_box.append($('<p/>', {html: "Anzahl B&auml;ume: " + len}))
+		}
+		
+		var newestTimestamp = 0;
+		for (var key in this.trees) {
+	   var obj = this.trees[key];
+	   if (obj.timestamp > newestTimestamp) {
+		newestTimestamp = obj.timestamp;
+	   }
+	}
+		var dateUpdated = new Date( newestTimestamp).toGMTString();
+		
+		
+		var dateCreated = new Date( this.wiese.data.timestamp);
+		
+		if(newestTimestamp == 0) {
+			dateUpdated = "-";
+		}
+		info_box.append($('<p>', {text: "Bearbeitet: " + dateUpdated}));
+		
+		
+		
+		info_box.append($('<p/>', {text: "Erstellt: " +  dateCreated.toGMTString()}));
+		info_box.append($('<p/>', {text: "Wetter??"}));
 
-WieseSubmenuHelper.prototype.get_submenu_box = function(menu){
-	var id = menu.submenu;
-	return $('#' + this.boxes_id).find("#sub_menu_" + id);
-}
+		if(sessionStorage.getItem('user') == 'Offline') {
+			$('#buttonOrchardOffline').attr('disabled', 'disabled' );
+		}
 
-WieseSubmenuHelper.prototype.make_all_unactive = function(){
-	this.menu_keys().forEach(function(menu_key){
-		var menu = this.menus[menu_key];
+		$('#buttonOrchardOffline').click(function(){
+			this.wiese.save_map_for_offline_use();
+			makeAvailableOffline(this.wiese.name, this.wiese.data);
+			$('#myModalOrchard').modal('show');
+		}.bind(this));
+	};
 
-		var btn = this.get_submenu_btn(menu);
-		var text = btn.text();
-			btn.removeClass("active");
-			btn.html($('<a/>', {href: "#", text: text}));
-			btn.unbind( "click" );
-			btn.click(function(){
-				this.make_active(menu)
+	WieseSubmenuHelper.prototype.fill_baume_box = function(){
+		var renderd_trees = RenderHelper.get_renderd_trees(this.wiese);
+		var ele;
+
+		if(renderd_trees.length > 0){
+			ele = $('<div/>', {class: "list-group"});
+
+			renderd_trees.forEach(function(rend_tree){
+				ele.append(rend_tree);
+			});
+
+		}else{
+			//wiese besitzt keine baume
+			ele = $('<div/>', {class: "well", text: "Sie haben noch keine Bäume auf ihrer Wiese plaziert"});
+		}
+
+		$('#trees_list').html(ele)
+	}
+
+	WieseSubmenuHelper.prototype.fill_filter_box = function(){
+	var allSorts = getAllSortNames(this.trees)
+
+	for (var i = 0; i < allSorts.length; i++) {
+	var checkboxDiv = document.createElement('div');
+	checkboxDiv.className = "checkbox";
+
+	var checkbox = document.createElement('input');
+	checkbox.type = "checkbox";
+	checkbox.name = "filterSort";
+	checkbox.value = allSorts[i];
+	var label = document.createElement('label')
+	var container = $('#sub_menu_filter');
+	container.append(checkboxDiv);
+	checkboxDiv.appendChild(label);
+	label.appendChild(checkbox);
+	label.appendChild(document.createTextNode(allSorts[i]));
+	}
+
+	}
+
+	WieseSubmenuHelper.prototype.get_submenu_btn = function(menu){
+		var data_attr = menu.submenu;
+		return $('*[data-submenu="' +  data_attr + '"]');
+	}
+
+	WieseSubmenuHelper.prototype.get_submenu_box = function(menu){
+		var id = menu.submenu;
+		return $('#' + this.boxes_id).find("#sub_menu_" + id);
+	}
+
+	WieseSubmenuHelper.prototype.make_all_unactive = function(){
+		this.menu_keys().forEach(function(menu_key){
+			var menu = this.menus[menu_key];
+
+			var btn = this.get_submenu_btn(menu);
+			var text = btn.text();
+				btn.removeClass("active");
+				btn.html($('<a/>', {href: "#", text: text}));
+				btn.unbind( "click" );
+				btn.click(function(){
+					this.make_active(menu)
+				}.bind(this));
+
+			var submenu_box = this.get_submenu_box(menu);
+				submenu_box.hide();
+		}.bind(this));
+	}
+
+	WieseSubmenuHelper.prototype.make_active = function(menu){
+		//erst alle menü unterpunkte ausblenden
+		this.make_all_unactive();
+		//dann nur den gewünschten zeigen
+		var active_btn = this.get_submenu_btn(menu);
+		var text = active_btn.text();
+			active_btn.addClass("active");
+			active_btn.unbind( "click" );
+			active_btn.html(text);
+
+		var active_box = this.get_submenu_box(menu);
+			active_box.show();
+	}
+
+
+	WieseSubmenuHelper.prototype.show_info = function() {
+		this.make_active(this.menus.info);
+	};
+
+	WieseSubmenuHelper.prototype.show_single_tree = function(tree_key, wiese){
+		var old_html = $('#right_menu_map').html();
+
+
+		$('#right_menu_map').load("html/tree/tree_menu.html", function(){
+			//wenn schliesen geklickt wird dann zuruck zum menu
+			$('#cancel_tree_view').click(function(){
+				$('#right_menu_map').html(old_html);
+			});
+			//bei bearbeiten zum Baum leiten
+			$('#edit_tree').click(function(){
+				var tree_obj = new Tree()
+					tree_obj.from_server_obj(this.trees, tree_key);
+					tree_obj.wiese = wiese;
+
+				new TreeForm(tree_obj).show_form();
 			}.bind(this));
 
-		var submenu_box = this.get_submenu_box(menu);
-			submenu_box.hide();
-	}.bind(this));
-}
-
-WieseSubmenuHelper.prototype.make_active = function(menu){
-	//erst alle menü unterpunkte ausblenden
-	this.make_all_unactive();
-	//dann nur den gewünschten zeigen
-	var active_btn = this.get_submenu_btn(menu);
-	var text = active_btn.text();
-		active_btn.addClass("active");
-		active_btn.unbind( "click" );
-		active_btn.html(text);
-
-	var active_box = this.get_submenu_box(menu);
-		active_box.show();
-}
-
-
-WieseSubmenuHelper.prototype.show_info = function() {
-	this.make_active(this.menus.info);
-};
-
-WieseSubmenuHelper.prototype.show_single_tree = function(tree_key){
-	var old_html = $('#right_menu_map').html();
-
-
-	$('#right_menu_map').load("html/tree/tree_menu.html", function(){
-		//wenn schliesen geklickt wird dann zuruck zum menu
-		$('#cancel_tree_view').click(function(){
-			$('#right_menu_map').html(old_html);
-		});
-		//bei bearbeiten zum Baum leiten
-		$('#edit_tree').click(function(){
-			var tree_obj = new Tree()
-				tree_obj.from_server_obj(this.trees, tree_key);
-				tree_obj.wiese = wiese;
-
-			new TreeForm(tree_obj).show_form();
 		}.bind(this));
-
-	}.bind(this));
-}
+	}
 
 
-function getNewestTreeTimestamp() {
 
-}
-
+}
--- www/js/wiese/wiese_place_trees.js
+++ www/js/wiese/wiese_place_trees.js
@@ -1,76 +1,119 @@
-Wiese.prototype.init_on_click_on_map = function(){
-	//Initialisiert was passiert wenn auf Map geklickt wird
-	this.map.on("click", function(e) {
-		this.map.forEachFeatureAtPixel(e.pixel, function (feature, layer) {
+{
+	Wiese.prototype.init_on_click_on_map = function(){
+		//Initialisiert was passiert wenn auf Map geklickt wird
+		this.map.on("click", function(e) {
+			this.map.forEachFeatureAtPixel(e.pixel, function (feature, layer) {
 
-		   	if(feature.get("name_type") == "tree"){
-		   		//show tree in menu at right side
-		   		this.submenu.show_single_tree(feature.get("key"));
-		   	}
+			   	if(feature.get("name_type") == "tree"){
+			   		//show tree in menu at right side
+			   		this.submenu.show_single_tree(feature.get("key"), this);
+					
+			   	}
 
+			}.bind(this));
 		}.bind(this));
-	}.bind(this));
-}
+	}
 
-Wiese.prototype.place_trees_on_map = function(){
-	if(this.data.trees){
-		this.init_on_click_on_map();
+	Wiese.prototype.place_trees_on_map = function(treeList){
+		
+		//remove previous tree layers
+		for (var i = 0; i < this.layers.length; i++) {
+	    this.map.removeLayer(this.layers[i]);
+	}
+		
+		
 
-		var vectorSource = new ol.source.Vector({});
+		if(treeList) {
+			this.init_on_click_on_map();
 
-		Object.keys(this.data.trees).forEach(function(treeKey){
+			var vectorSource = new ol.source.Vector({});
 
-			var tree = this.data.trees[treeKey];
-			//check that tree has real coordinates
-			if(!( isNaN(parseFloat(tree.lon)) ||  isNaN(parseFloat(tree.lat)) )){
+			Object.keys(treeList).forEach(function(treeKey){
 
-				var lon = parseFloat(tree.lon);
-				var lat = parseFloat(tree.lat);
+				var tree = treeList[treeKey];
+				//check that tree has real coordinates
+				if(!( isNaN(parseFloat(tree.lon)) ||  isNaN(parseFloat(tree.lat)) )){
 
-				var treeDot = ol.proj.fromLonLat([lon,lat]);
-				var treePoint = new ol.geom.Point(treeDot);
+					var lon = parseFloat(tree.lon);
+					var lat = parseFloat(tree.lat);
 
-				var l1 = treeDot[0];
-				var l2 = treeDot[1];
+					var treeDot = ol.proj.fromLonLat([lon,lat]);
+					var treePoint = new ol.geom.Point(treeDot);
 
-				var extent = [l1 - 5, l2 - 5,l1 + 5,l2 + 5];
+					var l1 = treeDot[0];
+					var l2 = treeDot[1];
 
-				//Bilder welche random als Baum eingesetzt werden
-				var images = ['Apfel1.png',
-							  'Apfel2.png',
-							  'Birne1.png',
-							  'Birne2.png',
-							  'Kirsche1.png',
-							  'Kirsche2.png',
-							  'Pflaume1.png',
-							  'Pflaume2.png'];
+					var extent = [l1 - 5, l2 - 5,l1 + 5,l2 + 5];
 
-			 	var layer = new ol.layer.Image({
-		            source: new ol.source.ImageStatic({
-		              url: "img/treeicons/" + images[Math.floor(Math.random() * 8)],
-		              imageExtent: extent
-		            })
-		        });
-		        this.map.addLayer(layer);
+					//Bilder welche random als Baum eingesetzt werden
+					var images = ['Apfel1.png',
+								  'Apfel2.png',
+								  'Birne1.png',
+								  'Birne2.png',
+								  'Kirsche1.png',
+								  'Kirsche2.png',
+								  'Pflaume1.png',
+								  'Pflaume2.png'];
 
-		        //Dient dazu damit es einen auschnitt gibt wo der User später draufklciken kann
-		        var feature = new ol.Feature({
-				  geometry: new ol.geom.Polygon.fromExtent(extent),
-				  key: treeKey,
-				  name_type: 'tree'
-				});
+				 	var layer = new ol.layer.Image({
+			            source: new ol.source.ImageStatic({
+			           //   url: "img/treeicons/" + images[Math.floor(Math.random() * 8)],
+							url: "img/treeicons/" + tree.sortname + "1.png",
+			              imageExtent: extent
+			            })
+			        });
+					
+					 var layers = this.layers;
+					layers[layers.length] = layer;
+			
+					this["layers"] = layers;
+					console.log(this);
+					
+					
+					
+					
+			        this.map.addLayer(layer);
+					layer.set("name", "TreeLayer");
+					console.log(layer.getProperties().name);
 
-		        vectorSource.addFeature(feature);
+			        //Dient dazu damit es einen auschnitt gibt wo der User später draufklciken kann
+			        var feature = new ol.Feature({
+					  geometry: new ol.geom.Polygon.fromExtent(extent),
+					  key: treeKey,
+					  name_type: 'tree',
 
-			}
+					});
 
-		}.bind(this));
+			        vectorSource.addFeature(feature);
+			
+					
+
+				}
+
+			}.bind(this));
 
 
-		var vector = new ol.layer.Vector({
-		  source: vectorSource,
-		  opacity: 0
-		});
-		this.map.addLayer(vector);
-	};
-}
+			var vector = new ol.layer.Vector({
+			  source: vectorSource,
+			  opacity: 0
+			});
+			this.map.addLayer(vector);
+			
+			
+			
+		};
+		
+		
+		
+		
+		
+
+		}
+
+
+
+
+
+
+
+}
