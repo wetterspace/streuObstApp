213bedcffc57e6061d82e3d26702a545cf20b5f9
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2016-02-09 09:33:35 +0100

    add obstarten editor

--- www/js/obst/obst.js
+++ www/js/obst/obst.js
@@ -6,12 +6,20 @@ var Obst = {
 };
 
 
-Obst.getArten = function () {
-	return Object.keys(Obst.Arten);
+Obst.getArten = function (obstarten) {
+	if(obstarten){
+	   return Object.keys(obstarten);
+	}else{
+	   return Object.keys(Obst.Arten);
+	}
 };
 
-Obst.getSorten = function(art){
-	return Obst.Arten[art];
+Obst.getSorten = function(art, obstarten){
+	if(obstarten){
+		return obstarten[art];
+	}else{
+		return Obst.Arten[art];
+	}
 }
 
 
--- www/js/tree/tree.js
+++ www/js/tree/tree.js
@@ -139,7 +139,7 @@ Tree.prototype.save = function() {
 
 
 		saveOffline(this.id, this);
-		new Wiese(this.wieseName).show();
+		new Wiese(this.wieseName, this.wiese.obstarten).show();
 
 
 	}else {
