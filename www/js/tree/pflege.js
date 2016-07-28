00335128efe0c9aa72539968a61d1a77ac9fe6a8
Florian Weber <s0544837@htw-berlin.de>
2016-07-28 11:48:29 +0200

    Fix code style

--- www/js/tree/pflege.js
+++ www/js/tree/pflege.js
@@ -1,40 +1,39 @@
 var PflegeAttr = {
+    id: {
+        // gets set in PflegeForm when Is saved
+        // get_pflegezustaende_to_save
+        // selbe wie bei Firebase um bereits gespeicherte Pflegezustände zu finden
+        id: 'created_at_id',
+        title: 'reated_at_id'
+    },
 
-	id: {
-		//gets set in PflegeForm when Is saved
-		//get_pflegezustaende_to_save
-		//selbe wie bei Firebase um bereits gespeicherte Pflegezustände zu finden
-		id: "created_at_id",
-		title: "reated_at_id"
-	},
+    krone_beschnitten: {
+        id: 'krone_beschnitten',
+        title: 'Krone Beschnitten'
+    },
 
-	krone_beschnitten: {
-			id: "krone_beschnitten",
-			title: "Krone Beschnitten"
-	},
+    hohe_der_krone: {
+        id: 'höhe_der_krone',
+        title: 'Höhe der Krone'
+    },
 
-	hohe_der_krone: {
-			id: "höhe_der_krone",
-			title: "Höhe der Krone"
-	},
+    baumstamm_gereinigt: {
+        id: 'baumstamm_gereinigt',
+        title: 'Baumstamm gereinigt'
+    },
 
-	baumstamm_gereinigt: {
-			id: "baumstamm_gereinigt",
-			title: "Baumstamm gereinigt"
-	},
+    baumstamm_gekalket: {
+        id: 'baumstamm_gekalket',
+        title: 'Baumstamm gekalkt'
+    },
 
-	baumstamm_gekalket: {
-			id: "baumstamm_gekalket",
-			title: "Baumstamm gekalkt"
-	},
+    schaedline: {
+        id: 'schädlinge',
+        title: 'Schädlinge'
+    },
 
-	schaedline: {
-			id: "schädlinge",
-			title: "Schädlinge"
-	},
-
-	verbiss_spuren: {
-			id: "verbiss_spuren",
-			title: "Verbissspuren"
-	}
+    verbiss_spuren: {
+        id: 'verbiss_spuren',
+        title: 'Verbissspuren'
+    }
 };
